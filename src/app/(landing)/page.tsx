50e1f9bd54c09d4b858a32812592154757dcd7ba
Advaita <148793147+01adv@users.noreply.github.com>
2024-10-09 22:00:09 +0000

    feat: dashboard and pricing added

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,25 @@
+import dynamic from "next/dynamic"
 import CallToAction from "./_components/CallToAction"
+import DashboardSnippet from "./_components/dashboard-snippet"
+
+// const PricingSection = dynamic(()=> import("./_components/PricingSection").then(component) => component.PricingSection),)
+const PricingSection = dynamic(
+    () =>
+        import("./_components/pricing").then(
+            (component) => component.PricingSection,
+        ),
+    { ssr: true },
+)
 
 type Props = {}
-
 const page = (props: Props) => {
     return (
         <main className="md:px-10 py-20 flex flex-col gap-36">
             <div>
                 <CallToAction />
+                <DashboardSnippet />
             </div>
+            <PricingSection />
         </main>
     )
 }
